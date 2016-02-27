30ef4139490b48fd11dd55cc9439f68c07412022
Alison <alison@johnston.name>
2016-02-27 23:58:18 +0000

    update

--- src/code/B_ArrayHasTwoElementsThatAddToNumber.java
+++ src/code/B_ArrayHasTwoElementsThatAddToNumber.java
@@ -1,23 +1,24 @@
 import java.util.Arrays;
 
 public class B_ArrayHasTwoElementsThatAddToNumber {
-    public static boolean check(int[] numbers, int toFind){   
-    	if(numbers.length == 0) {
-    		return false;
-    	}     
+    public static boolean check(int[] numbers, int toFind){
+        if(numbers.length == 0) {
+        	return false;
+        }
         Arrays.sort(numbers);
         int low = 0;
         int high = numbers.length-1;
-        while(low < high){
-        	int sum = numbers[low] + numbers[high];
-	        if(sum == toFind){
-	        	return true;
-	        } else if (sum < toFind) {
-	        	low++;
-	        } else {
-	        	high--;
-	        }
+        while(low<high){
+        	int sum = numbers[low]+numbers[high];        	
+        	if(sum == toFind){
+        		return true;
+        	} else if(sum<toFind) {
+        		low++;
+        	} else if (sum>toFind) {
+        		high--;
+        	}
         }
+        // in case only 1 element
         return numbers[low] == toFind;
     }
 }
