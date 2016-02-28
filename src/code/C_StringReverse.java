d6f0ae0364531f63aecdd140e8843027a123db3f
Alison <alison@johnston.name>
2016-02-28 00:04:08 +0000

    C string reverse

---
+++ src/code/C_StringReverse.java
@@ -0,0 +1,6 @@
+
+public class C_StringReverse {
+    public static String get(String forward) {
+        return forward;
+    }
+}
---
+++ src/test/C_StringReverseSpec.groovy
@@ -0,0 +1,49 @@
+import spock.lang.Specification
+
+class C_StringReverseSpec extends Specification {
+
+    def'reverses string ok'(){
+        given:
+        String forward = 'abcdef'
+        String reverse = 'fedcba'
+
+        expect:
+        C_StringReverse.get(forward) == reverse
+    }
+
+    def'empty string returns empty string'(){
+        given:
+        String forward = ''
+        String reverse = ''
+
+        expect:
+        C_StringReverse.get(forward) == reverse
+    }
+
+    def'null string returns empty null'(){
+        given:
+        String forward = null
+        String reverse = null
+
+        expect:
+        C_StringReverse.get(forward) == reverse
+    }
+
+    def'single character string returns character'(){
+        given:
+        String forward = 'f'
+        String reverse = 'f'
+
+        expect:
+        C_StringReverse.get(forward) == reverse
+    }
+
+    def'mixed case returns reversed string'(){
+        given:
+        String forward = 'AbDgT'
+        String reverse = 'TgDbA'
+
+        expect:
+        C_StringReverse.get(forward) == reverse
+    }
+}
