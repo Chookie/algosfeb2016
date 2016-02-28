fa715ceb7c86650a978243a510599fed4afb628a
Alison <alison@johnston.name>
2016-02-28 21:42:48 +0000

    J Anagram

--- src/code/J_Anagram.java
+++ src/code/J_Anagram.java
@@ -1,6 +1,55 @@
+import java.util.Arrays;
 
 public class J_Anagram {
+
+	/**
+	* Sort arrays
+	*/
     public static boolean methodOne(String word, String anagram) {
-        return false;
+        
+        char[] wordArray = word.toLowerCase().toCharArray();
+        char[] anagramArray = anagram.toLowerCase().toCharArray();
+
+        Arrays.sort(wordArray);
+        Arrays.sort(anagramArray);
+
+        return Arrays.equals(wordArray,anagramArray);
+    }
+
+	/**
+	* Use temp string
+	*/
+    public static boolean methodTwo(String word, String anagram) {
+        if(word.length() != anagram.length() ){
+            return false;
+        }
+
+        int i = 0;
+        while(i < word.length() && word.length() > 0){
+        	char ch = word.toLowerCase().charAt(i);
+        	int loc = anagram.toLowerCase().indexOf(ch);
+        	if(loc == -1){
+        		return false;
+        	}
+            anagram = anagram.substring(0,loc) + anagram.substring(loc+1,anagram.length());
+            i++;
+        }
+        return anagram.length() == 0;
+    }
+
+
+    /**
+     * Use same string abd toarray
+     */
+    public static boolean methodThree(String word, String anagram) {
+
+        for(char ch : word.toCharArray()){
+            int loc = anagram.toLowerCase().indexOf(ch);
+            if(loc == -1){
+                return false;
+            }
+            anagram = anagram.substring(0,loc) + anagram.substring(loc+1,anagram.length());
+        }
+        return anagram.length() == 0;
     }
 }
--- src/test/J_AnagramSpec.groovy
+++ src/test/J_AnagramSpec.groovy
@@ -5,14 +5,54 @@ import spock.lang.Unroll
 /**
  * Test if anagram of word based on letters only, not words and case insensitive.
  */
+@SuppressWarnings("GroovyPointlessBoolean")
 class J_AnagramSpec extends Specification {
 
-    @SuppressWarnings("GroovyPointlessBoolean")
     @Unroll
-    def'#anagram rotis anagram of #word returns #result'(){
+    def'methodOne #anagram is anagram of #word returns #result'(){
 
         expect:
         J_Anagram.methodOne(word,anagram) == result
+
+        where:
+        word            |  anagram          || result
+        'word'          |  'wrdo'           || true
+        'mary'          |  'army'           || true
+        'mary'          |  'Army'           || true
+        'stop'          |  'tops'           || true
+        'boat'          |  'btoa'           || true
+        'a'             |  'a'              || true
+        'fill'          |  'fil'            || false
+        'b'             |  'pure'           || false
+        'ccc'           |  'ccccccc'        || false
+        'sleep'         |  'slep'           || false
+    }
+
+    @Unroll
+    def'methodTwo #anagram is anagram of #word returns #result'(){
+
+        expect:
+        J_Anagram.methodTwo(word,anagram) == result
+
+        where:
+        word            |  anagram          || result
+        'word'          |  'wrdo'           || true
+        'mary'          |  'army'           || true
+        'mary'          |  'Army'           || true
+        'stop'          |  'tops'           || true
+        'boat'          |  'btoa'           || true
+        'a'             |  'a'              || true
+        'fill'          |  'fil'            || false
+        'b'             |  'pure'           || false
+        'ccc'           |  'ccccccc'        || false
+        'sleep'         |  'slep'           || false
+    }
+
+    @Unroll
+    def'methodThree #anagram is anagram of #word returns #result'(){
+
+        expect:
+        J_Anagram.methodThree(word,anagram) == result
 
         where:
         word            |  anagram          || result
