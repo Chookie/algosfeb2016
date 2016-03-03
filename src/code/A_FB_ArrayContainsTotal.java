2c541a1b49e7de2b58c43e040b9d11ad2578c6a0
Alison <alison@johnston.name>
2016-03-03 20:08:57 +0000

    array contains

--- src/code/A_FB_ArrayContainsTotal.java
+++ src/code/A_FB_ArrayContainsTotal.java
@@ -1,6 +1,27 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class A_FB_ArrayContainsTotal {
+
+    /**
+     * This only works for positive targets
+     */
     public static boolean arrayContains(int[] array, int target) {
+        Set<Integer> set = new HashSet<>();
+        int sum = 0;
+        for (int item : array) {
+            sum += item;
+            if (sum == target) {
+                return true;
+            } else if (sum < target) {
+                set.add(sum);
+            } else {
+                int diff = sum - target;
+                if (set.contains(diff)) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
